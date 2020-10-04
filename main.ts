efb6675b81af15b37ccaced4f06d00afbe0bae53
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2020-10-05 00:50:45 +0200

    implemented the ackermann function

--- main.ts
+++ main.ts
@@ -1,35 +1,40 @@
-import {add, sub} from "./stringMath.ts";
+import { add, sub } from "./stringMath.ts";
 
 console.log(ackermann(5, 3));
-console.log(597 - 1109);
-console.log(sub("597", "1109"));
 
 function ackermann(n: number, m: number): string {
   let str = toAString(n, m);
   while (str.includes("a")) {
+    console.log(str);
     let innerA = str.substring(str.lastIndexOf("a"));
     innerA = innerA.substring(0, innerA.indexOf(")") + 1);
     str = str.substring(0, str.lastIndexOf("a")) + a(innerA) + str.substring(str.lastIndexOf("a") + innerA.length);
-    break;
+    // break;
   }
   return str;
 }
 
 function toAString(n: string | number, m: string | number): string {
-  return "a(" + n + ", " + m + ")";
+  return `a(${n}, ${m})`;
 }
 
 function a(str: string): string {
   const n = str.substring(2, str.indexOf(",")).trim();
-  const m = str.substring(str.indexOf(",") + 1, str.length).trim();
-  console.log({ n, m });
-  return str;
+  const m = str.substring(str.indexOf(",") + 1, str.length-1).trim();
+  // console.log({ n, m });
+  if (n === "0")
+    return add(m, "1");
+  if (m === "0")
+    return toAString(sub(n, "1"), "1");
+  // console.log(m, "-", 1);
+  // console.log(sub(m, "1"));
+  return toAString(sub(n, "1"), toAString(n, sub(m, "1")));
 }
 
 function ackermann_simple(n: number, m: number): number {
   if (n === 0)
     return m + 1;
-  if (m == 0)
+  if (m === 0)
     return ackermann_simple(n - 1, 1);
   return ackermann_simple(n - 1, ackermann_simple(n, m - 1));
 }
