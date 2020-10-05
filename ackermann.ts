92a47e60fe03d220b609348eb8d7e156784ff165
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2020-10-05 15:21:47 +0200

    moved the ackermann function to ackermann.ts
    - created a Deno script to call ackermann from the command line with command line arguments
    
    Note: You can install the script using `deno install`

--- ackermann.ts
+++ ackermann.ts
@@ -1,11 +1,9 @@
 import { add, sub } from "./stringMath.ts";
 
-console.log(ackermann(5, 3));
-
-function ackermann(n: number, m: number): string {
+export function ackermann(n: number, m: number, logging?: boolean): string {
   let str = toAString(n, m);
   while (str.includes("a")) {
-    console.log(str);
+    if(logging) console.log(str);
     let innerA = str.substring(str.lastIndexOf("a"));
     innerA = innerA.substring(0, innerA.indexOf(")") + 1);
     str = str.substring(0, str.lastIndexOf("a")) + a(innerA) + str.substring(str.lastIndexOf("a") + innerA.length);
@@ -13,6 +11,8 @@ function ackermann(n: number, m: number): string {
   }
   return str;
 }
+
+export default ackermann;
 
 function toAString(n: string | number, m: string | number): string {
   return `a(${n}, ${m})`;
@@ -31,7 +31,7 @@ function a(str: string): string {
   return toAString(sub(n, "1"), toAString(n, sub(m, "1")));
 }
 
-function ackermann_simple(n: number, m: number): number {
+export function ackermann_simple(n: number, m: number): number {
   if (n === 0)
     return m + 1;
   if (m === 0)
---
+++ console.ts
@@ -0,0 +1,15 @@
+import { ackermann, ackermann_simple } from "./ackermann.ts";
+console.log("Now running ackermann PRO - developed by JustAnotherJavaProgrammer");
+const n = parseInt(Deno.args[0], 10);
+const m = parseInt(Deno.args[1], 10);
+const logging = Deno.args.includes("-l") || Deno.args.includes("--logging");
+if (Deno.args.length < 2 || Number.isNaN(n) || Number.isNaN(m)) {
+    console.info("usage: ackermann [n] [m] OPTIONS\nValidoptions are:\n-logging, -l\tenable logging of intermediate steps");
+    Deno.exit(1);
+}
+if (Deno.args[0].includes(".") || Deno.args[1].includes(".") || n < 0 || m < 0) {
+    console.error("ERROR: The ackermann function only accepts positive integers.");
+    Deno.exit(2);
+}
+
+console.log(ackermann(parseInt(Deno.args[0]), parseInt(Deno.args[1]), logging));
